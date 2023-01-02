ee4c535d1dc9919191b352f9d14190090188652a
Abcd Efgh <vgirimaji@gmail.com>
2023-01-02 08:23:16 +0530

    "Hello world" socket program

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,18 @@
 const std = @import("std");
+const net = std.net;
+const StreamServer = std.net.StreamServer;
+const Address = std.net.Address;
 
 pub fn main() !void {
-    std.debug.print("Hello, world\n", .{});
+    var my_server = StreamServer.init(.{ .reuse_address = true });
+    defer my_server.close();
+
+    const address = try Address.resolveIp("127.0.0.1", 8088);
+    try my_server.listen(address);
+
+    while (true) {
+        const connection = try my_server.accept();
+        try connection.stream.writer().print("hello, world", .{});
+        connection.stream.close();
+    }
 }
